805e40a2e85f49d36d6e2f0425219c59a7704ca3
Pragati <pblkjh@gmail.com>
2021-03-16 16:57:26 +0530

    part6

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.use(bp.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 var item = "";
-var items = [];
+var items = ["Buy food", "Eat food"];
 
 app.get('/', (req, res) => {
     var today = new Date();
