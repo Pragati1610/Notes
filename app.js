b4e8e3fdf8c7433c026b77cd6e0d65c204f26cff
Pragati <pblkjh@gmail.com>
2021-03-16 16:48:37 +0530

    part5

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const bp = require("body-parser");
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(bp.urlencoded({ extended: true }));
+
+var item = "";
+var items = [];
 
 app.get('/', (req, res) => {
     var today = new Date();
@@ -14,11 +18,22 @@ app.get('/', (req, res) => {
     };
     let day = today.toLocaleDateString("en-US", options);
 
-    res.render('list', { kindOfDay: day });
+    res.render('list', { kindOfDay: day, newListItems: items });
 });
 
-
+app.post('/', (req, res) => {
+    item = req.body.newItem;
+    // res.render('list', { newListItem: item });
+    items.push(item);
+    res.redirect("/");
+});
 
 app.listen(3000, () => {
     console.log("server started");
 });
+
+
+// P1: newListItem not defined before in get request
+// p2: item is created only in post request (scope is different)
+// p3: only 1 item is showed up i.e. the newest one
+// p4: rendered as a single list item
