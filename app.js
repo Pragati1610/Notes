ad7eed0fda4d8fcdaf0ad4bd67cdd390ee2639fd
Pragati <pblkjh@gmail.com>
2021-03-16 16:23:30 +0530

    part 2

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     var today = new Date();
-
-    if (today.getDay() === 2 || today.getDay() === 0) {
-        res.send("<h1>Weekend</h1>");
+    let day;
+    if (today.getDay() === 6 || today.getDay() === 0) {
+        day = "Weekend";
     } else {
-        res.send("Weekday");
+        day = "Weekday";
     }
+    res.render('list', { kindOfDay: day });
 });
 
 app.listen(3000, () => {
